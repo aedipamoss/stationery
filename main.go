ebfd4a0dda9382e332b8e5fd0cee77fa07d7c0d5
Ædipa Moss <24195775+aedipamoss@users.noreply.github.com>
2018-02-22 23:54:10 +0900

    First take at generation
    Read files in
    Run them through blackfriday (markdown parser)
    Write to output files

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/aedipamoss/stationery/config"
+	blackfriday "gopkg.in/russross/blackfriday.v2"
 )
 
 func main() {
@@ -18,7 +21,32 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("files: %v", files)
+	_, err = os.Stat(config.Output)
+	if err != nil && os.IsNotExist(err) {
+		fmt.Printf("Making output dir: %v\n", config.Output)
+		os.Mkdir(config.Output, 0777)
+	}
 
-	fmt.Println("Time to write!")
+	for _, file := range files {
+		src := config.Source + "/" + file.Name()
+		base := filepath.Ext(src)
+		name := file.Name()[0 : len(file.Name())-len(base)]
+		path := config.Output + "/" + name + ".html"
+
+		content, err := ioutil.ReadFile(src)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		out := blackfriday.Run(content)
+
+		err = ioutil.WriteFile(path, out, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Wrote: ", path)
+		//fmt.Printf("File contents: %s\n", content)
+	}
+
+	fmt.Println("Done!")
 }
